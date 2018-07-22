e765a0b8bb537fcbcc74547c01e73cb869907dc9
jangom2ok <jangom2ok@gmail.com>
2018-07-22 14:46:37 +0900

    #14 add EventDetailViewController

---
+++ OkaEvent_iOS/EventDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventDetailViewController.swift
+//  OkaEvent_iOS
+//
+//  Created by talkie on 2018/07/22.
+//  Copyright © 2018年 talkie. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
